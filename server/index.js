13faf41e86eb10afcd648aa91c625511fa104c1d
Roudi22 <roudi.alzkemi@gmail.com>
2024-07-12 18:08:14 +0200

    fix

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use("/uploads/place-photos", express.static(path.join(path.resolve(), "/uplo
 
 // use cors
 app.use(cors({
-  origin: 'https://booka-air.netlify.app/',
+  origin: 'http://localhost:5173',
   credentials: true,
 }));
 app.get('/test', (req, res) => {
